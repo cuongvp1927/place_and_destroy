10073f0f9fee8ca3a130e2ef1159ef9ae9f8199a
Shamimhhhub <143575338+Shamimhhhub@users.noreply.github.com>
2024-05-10 17:05:35 +1000

    level2 - movement animation

--- Assets/Script/BoxChangeColor.cs
+++ Assets/Script/BoxChangeColor.cs
@@ -14,18 +14,56 @@ public class BoxChangeColor : MonoBehaviour
 
     public List<colorBox> colorBoxes;
     private float timer;
+    private int curr;
+
+    void changePrefab(GameObject nextPrefab)
+    {
+        // deactivate anyother prefab
+        foreach (var box in colorBoxes)
+        {
+            if (box.preFab.tag != "Objective")
+            {
+                box.preFab.active = false;
+            }
+        }
+
+        // to activate the prefab
+        nextPrefab.active = true;
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         timer = 0;
+        curr = 0;
+        // deactivate anyother prefab
+        foreach (var box in colorBoxes)
+        {
+            if (box.preFab.tag != "Objective")
+            {
+                box.preFab.active = false;
+            }
+        }
+        /// activate the original prefab
+        colorBoxes[0].preFab.active = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-
+        if (timer >= colorBoxes[curr].nextTimer)
+        {
+            changePrefab(colorBoxes[curr].preFab);
+            curr++;
+            if (curr >= colorBoxes.Count)
+            {
+                curr = 0;
+            }
+            timer = 0;
+        }
 
     }
 }
