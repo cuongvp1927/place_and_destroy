3644700c32490bf66156503dcefb1492bebeef70
Killwound <166896635+Killwound@users.noreply.github.com>
2024-05-10 21:54:18 +1000

    1

---
+++ Assets/Script/LevelSelecter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startmenu : MonoBehaviour
+{
+    public void level1()
+    {
+        SceneManager.LoadScene("Level 1");
+
+    }
+    public void level2()
+    {
+        SceneManager.LoadScene("Level_2");
+
+    }
+
+    public void level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void level4()
+    {
+        SceneManager.LoadScene("Level4Scn");
+    }
+
+    public void levelTutorial()
+    {
+        SceneManager.LoadScene("Tutorial Scn");
+    }
+}
--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
+<<<<<<< Updated upstream
 {
+=======
+{private  int previousIndex;
+    private void Start()
+    {
+        previousIndex = SceneManager.GetActiveScene().buildIndex - 1;
+    }
+
+>>>>>>> Stashed changes
     public void SceneLoad(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
