6563e92580978338447a23647921a62231515f20
Killwound <166896635+Killwound@users.noreply.github.com>
2024-05-10 22:06:50 +1000

    level select basic

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
-<<<<<<< Updated upstream
-{
-<<<<<<< HEAD
-=======
+
 {private  int previousIndex;
-=======
-    [SerializeField] private  int previousIndex;
->>>>>>> level2
+
+
+
     private void Start()
     {
         previousIndex = SceneManager.GetActiveScene().buildIndex - 1;
     }
 
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> level2
+
     public void SceneLoad(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
